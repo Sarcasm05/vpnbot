effdad5dc068212f778f5c0b267cf085f4761e92
Brdsky <ramazanovrustam98@icloud.com>
2018-05-30 02:02:39 +0300

    Update func.py

--- src/func.py
+++ src/func.py
@@ -1,31 +1,69 @@
 # -*- coding: utf-8 -*-
 import sqlite3
-import config
+from config import database_name
 import telebot
 from SQLite import liter
 
+def add_user(user_id):
+    bd = liter(database_name)
+    bd.add_user(user_id)
+    bd.close()
 
+def autorisation(user_id):
+    bd = liter(database_name)
+    if not bd.select_user(user_id):
+        return 0
+    else :
+        return 1
 
+def change_user_state(user_id, state):
+    bd = liter(database_name)
+    bd.update_user_state(user_id, state)
+    bd.close()
+
+def update_user_choice(user_id, choice):
+    bd = liter(database_name)
+    bd.update_user_choice(user_id, choice)
+    bd.close()
+
+def select_user_state(user_id):
+    bd = liter(database_name)
+    state = bd.select_user_state(user_id)
+    bd.close()
+    return state
+
+def select_vpn(choice):
+    bd = liter(database_name)
+    vpn = bd.select_vpn(choice)
+    bd.close()
+    return vpn
+    
 def country():       
-    bd = liter(config.database_name) 
+    bd = liter(database_name) 
     row = bd.select_country()
     bd.close()
     return row
 
-def state():
-    bd = liter(config.database_name) 
-    row = bd.select_state()
+def state(country):
+    bd = liter(database_name) 
+    row = bd.select_state(country)
     bd.close()
     return row
 
-def city():
-    bd = liter(config.database_name) 
-    row = bd.select_city()
+def city(state):
+    bd = liter(database_name) 
+    row = bd.select_city(state)
+    bd.close()
+    return row
+
+def our_choice(city):
+    bd = liter(database_name)
+    row = bd.our_choice(city)
     bd.close()
     return row
 
 def exist(call):
-    bd = liter(config.database_name) 
+    bd = liter(database_name) 
     if len(bd.exist(call)) == 0:
         bd.close()
         return False
@@ -33,10 +71,48 @@ def exist(call):
         bd.close()
         return True
 
+def in_country(call):
+    bd = liter(database_name)
+    if not bd.in_country(call):
+        bd.close()
+        return 0
+    else :
+        bd.close()
+        return 1
+
+def in_state(call):
+    bd = liter(database_name)
+    if not bd.in_state(call):
+        bd.close()
+        return 0
+    else :
+        bd.close()
+        return 1
+
+def in_city(call):
+    bd = liter(database_name)
+    if not bd.in_city(call):
+        bd.close()
+        return 0
+    else :
+        bd.close()
+        return 1
+
+def in_zip(call):
+    bd = liter(database_name)
+    if not bd.in_zip(call):
+        bd.close()
+        return 0
+    else :
+        bd.close()
+        return 1
+
 
 
 def create_keyboard(row):
     markup = telebot.types.InlineKeyboardMarkup()
-    for elem in row :
-        markup.add(telebot.types.InlineKeyboardButton(text = str(elem), callback_data = str(elem)))
+    myset = set(row)
+    print(myset)
+    for elem in myset :
+        markup.add(telebot.types.InlineKeyboardButton(text = elem[0], callback_data = elem[0]))
     return markup
