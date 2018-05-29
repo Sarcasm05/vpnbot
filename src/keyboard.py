01290c4b2d171f67dce9067aa9ba367aafea738d
Brdsky <ramazanovrustam98@icloud.com>
2018-05-30 02:02:09 +0300

    Update keyboard.py

--- src/keyboard.py
+++ src/keyboard.py
@@ -9,7 +9,9 @@ bot = telebot.TeleBot(config.token)
 class Keyboard:
     def __init__(self, bot):
         self.bot = bot
-
+    def hide(self):
+        markup = telebot.types.ReplyKeyboardRemove(True)
+        return markup
     def menu(self):
         menu_markup = telebot.types.ReplyKeyboardMarkup(True, True)
         menu_markup.row("В главное меню")
@@ -18,21 +20,23 @@ class Keyboard:
     def country(self):
         return func.create_keyboard(func.country())
 
-    def state(self):
-        return func.create_keyboard(func.state())
+    def state(self, call):
+        return func.create_keyboard(func.state(call))
 
-    def city(self):
-        return func.create_keyboard(func.city())
+    def city(self, call):
+        return func.create_keyboard(func.city(call))
 
+    def our_choice(self, call):
+        return func.create_keyboard(func.our_choice(call))
 
     def buy_menu(self):
         buy_markup = telebot.types.InlineKeyboardMarkup()
         buy_markup.add(telebot.types.InlineKeyboardButton(text = 'Выбрать страну', callback_data = 'country'))
-        buy_markup.add(telebot.types.InlineKeyboardButton(text = 'Выбрать область/штат', callback_data = 'state'))
-        buy_markup.add(telebot.types.InlineKeyboardButton(text = 'Выбрать город', callback_data = 'city'))
+        #buy_markup.add(telebot.types.InlineKeyboardButton(text = 'Выбрать область/штат', callback_data = 'state'))
+        #buy_markup.add(telebot.types.InlineKeyboardButton(text = 'Выбрать город', callback_data = 'city'))
         return buy_markup
 
     def main_menu(self):
-        menu_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+        menu_markup = telebot.types.ReplyKeyboardMarkup(True, True)
         menu_markup.row("Купить vpn")
         return menu_markup
