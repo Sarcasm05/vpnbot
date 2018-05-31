fe65caeb7c9403a2acc112447e659078b4552865
Brdsky <ramazanovrustam98@icloud.com>
2018-05-31 23:52:53 +0300

    dbwork.py updated

--- dbwork/dbwork.py
+++ dbwork/dbwork.py
@@ -10,20 +10,17 @@ def add_filename(filename, login, password, site, ip, continentCode, continentNa
     db.add_filename(filename, login, password, site, ip, continentCode, continentName, countryCode, countryName, isEuMember, currencyCode, currencyName, phonePrefix, languages, stateProv, district, city, geonameId, latitude, longitude, gmtOffset, timeZone, asNumber, asName, isp, organization, isCrawler, isProxy, threatLevel)
     db.close()
 
-def exist(word, string):
-    if word in string:
+def exist(name,string, word):
+    if string in word:
+        row = word.split(': ')
+        try:
+            name = row[1]
+            #name = row[1]
+            #print(name)
+        except IndexError as e:
+            name = row[0]
         
-        """
-        newrow = ""
-        flag = False
-        for elem in word:
-            if elem == ":":
-                flag = True
-            if flag == True:
-                newrow += elem
-        """
-        row = string.split(': ')
-        return row[1]
+        
 
 def csv_reader(file_obj):
     """
@@ -36,64 +33,121 @@ def csv_reader(file_obj):
         password = row[2]
         site = row[3]
         ip = row[4]
-        continentCode = "none"
-        continentName = "none"
-        countryCode = "none"
-        countryName = "none"
-        isEuMember = "none"
-        currencyCode = "none"
-        currencyName = "none"
-        phonePrefix = "none"
-        languages = "none"
-        stateProv = "none"
-        district = "none"
-        city = "none"
-        geonameId = "none"
-        latitude = "none"
-        longitude = "none"
-        gmtOffset = "none"
-        timeZone = "none"
-        asNumber = "none"
-        asName = "none"
-        isp = "none"
-        organization = "none"
-        isCrawler = "none"
-        isProxy = "none"
-        threatLevel = "none"
+        
+        continentCode = "4134312"
+        continentName = "4352421313"
+        countryCode = "543754323141"
+        countryName = "n4316351423"
+        isEuMember = "431623453143"
+        currencyCode = "13462421342"
+        currencyName = "136423152154"
+        phonePrefix = "321643243242142"
+        languages = "43554"
+        stateProv = "цкайцвс"
+        district = "321423выфпфцйЙ"
+        city = "ФЖДЛФЫОЛДОЛЫо"
+        geonameId = "ваыылодфьдфалац"
+        latitude = "432авфввфа"
+        longitude = "аптыфв"
+        gmtOffset = "фукафмффцуа"
+        timeZone = "лотвыил"
+        asNumber = "ыдлафыдтолфуаф"
+        asName = "оавыфы"
+        isp = "афылщзофваышощфаы"
+        organization = "втавыощфаыщшо"
+        isCrawler = "афыощфаозшщ"
+        isProxy = "азщвыаощшфыкшфпова"
+        threatLevel = "уцйа2ащш43"
+        
         for word in row:
-            continentCode = exist("continentCode", word)
-            continentName = exist("continentName", word)
-            countryCode = exist("countryCode", word)
-            scountryNametring = exist("countryName", word)
-            isEuMember = exist("isEuMember", word)
-            currencyCode = exist("currencyCode", word)
-            currencyName = exist("currencyName", word)
-            phonePrefix = exist("phonePrefix", word)
-            languages = exist("languages", word)
-            stateProv = exist("stateProv", word)
-            district = exist("district", word)
-            city = exist("city", word)
-            geonameId = exist("geonameId", word)
-            latitude = exist("latitude", word)
-            longitude = exist("longitude", word)
-            gmtOffset = exist("gmtOffset", word)
-            sttimeZonering = exist("timeZone", word)
-            asNumber = exist("asNumber", word)
-            asName = exist("asName", word)
-            isp = exist("isp", word)
-            organization = exist("organization", word)
-            isCrawler = exist("isCrawler", word)
-            isProxy = exist("isProxy", word)
-            asNumber = exist("asNumber", word)
-            threatLevel = exist("threatLevel", word)
+           
+            #exist(continentCode,"continentCode", word)
+            if "continentCode" in word:
+                row = word.split(': ')
+                continentCode = row[1]
+            if "continentName" in word:
+                row = word.split(': ')
+                continentName = row[1]
+            if "countryCode" in word:
+                row = word.split(': ')
+                countryCode = row[1]
+            if "countryName" in word:
+                row = word.split(': ')
+                countryName = row[1]
+            if "isEuMember" in word:
+                row = word.split(': ')
+                isEuMember = row[1]
+            if "currencyCode" in word:
+                row = word.split(': ')
+                currencyCode = row[1]
+            if "phonePrefix" in word:
+                row = word.split(': ')
+                phonePrefix = row[1]
+            if "languages" in word:
+                row = word.split(': ')
+                languages = row[1]
+            if "stateProv" in word:
+                row = word.split(': ')
+                stateProv = row[1]
+            if "district" in word:
+                row = word.split(': ')
+                district = row[1]
+            if "city" in word:
+                try:
+                    row = word.split(': ')
+                    city = row[1]
+                except IndexError as e:
+                    city = row[0]
+            if "geonameId" in word:
+                row = word.split(': ')
+                geonameId = row[1]
+            if "latitude" in word:
+                row = word.split(': ')
+                latitude = row[1]
+            if "longitude" in word:
+                row = word.split(': ')
+                longitude = row[1]
+            if "gmtOffset" in word:
+                row = word.split(': ')
+                gmtOffset = row[1]
+            if "timeZone" in word:
+                row = word.split(': ')
+                timeZone = row[1]
+            if "asNumber" in word:
+                row = word.split(': ')
+                asNumber = row[1]
+            if "asName" in word:
+                row = word.split(': ')
+                asName = row[1]
+            if "isp" in word:
+                try:
+                    row = word.split(': ')
+                    isp = row[1]
+                except IndexError as e:
+                    isp = row[0]
+            if "isCrawler" in word:
+                row = word.split(': ')
+                isCrawler = row[1]
+            if "isProxy" in word:
+                row = word.split(': ')
+                isProxy = row[1]
+            if "threatLevel" in word:
+                row = word.split(': ')
+                continentName = row[1]
+            if "continentName" in word:
+                row = word.split(': ')
+                continentName = row[1]
+            if "continentName" in word:
+                row = word.split(': ')
+                continentName = row[1]
+           
             
-        """
         add_filename(filename, login, password, site, ip, continentCode, continentName, 
                     countryCode, countryName, isEuMember, currencyCode, currencyName, 
                     phonePrefix, languages, stateProv, district, city, geonameId, latitude, 
                     longitude, gmtOffset, timeZone, asNumber, asName, isp, organization, 
                     isCrawler, isProxy, threatLevel)
-        """       
+              
  
 if __name__ == "__main__":
     csv_path = CSV_NAME
