69c316cdc9a41411e43103ac5950a570dce9bac1
monsherko <monsherko@yahoo.com>
2018-06-15 13:46:42 +0300

    update sqlite.py

--- src/core/SQLite.py
+++ src/core/SQLite.py
@@ -3,96 +3,99 @@
 #update monsherko
 import sqlite3
 
+import MySQLdb
+
 class Liter:
 
     def __init__(self, database):
-        self.connection = sqlite3.connect(database)
+        self.connection =  MySQLdb.connect(host='localhost',user='root', db='activeDB')
+
         self.cursor = self.connection.cursor()
 
     def select_all(self):
         #Получаем все строки
         with self.connection:
-            return self.cursor.execute('SELECT * FROM vpns').fetchall()
+            return self.cursor.execute('SELECT * FROM FileOVPN').fetchall()
 
     def select_single(self, rownum):
-        #Получаем одну строку с номером rownum из vpns
+        #Получаем одну строку с номером rownum из FileOVPN
         with self.connection:
-            return self.cursor.execute('SELECT * FROM vpns WHERE id = ?', (rownum,)).fetchall()[0]
+            return self.cursor.execute('SELECT * FROM FileOVPN WHERE id = ?', (rownum,)).fetchall()[0]
 
     def select_country(self):
         #выбираем все страны
         with self.connection:
-            return self.cursor.execute('SELECT countryName FROM vpns').fetchall()
-    
+            return self.cursor.execute('SELECT countryName FROM FileOVPN').fetchall()
+
     def in_country(self, call):
         with self.connection:
-            return self.cursor.execute('SELECT * FROM vpns WHERE countryName = ?', (call,)).fetchall()
+            return self.cursor.execute('SELECT * FROM FileOVPN WHERE countryName = ?', (call,)).fetchall()
 
     def select_state(self, country):
         #выбираем все штаты
         with self.connection:
-            return self.cursor.execute('SELECT stateProv FROM vpns WHERE countryName = ?',(country,)).fetchall()
+            return self.cursor.execute('SELECT stateProv FROM FileOVPN WHERE countryName = ?',(country,)).fetchall()
 
     def in_state(self, call):
         with self.connection:
-            return self.cursor.execute('SELECT * FROM vpns WHERE stateProv = ?', (call,)).fetchone()
+            return self.cursor.execute('SELECT * FROM FileOVPN WHERE stateProv = ?', (call,)).fetchone()
 
     def select_city(self, state):
         #выбираем все города
         with self.connection:
-            return self.cursor.execute('SELECT city FROM vpns WHERE stateProv = ?',(state,)).fetchall()      
+            return self.cursor.execute('SELECT city FROM FileOVPN WHERE stateProv = ?',(state,)).fetchall()
 
     def in_city(self, call):
         with self.connection:
-            return self.cursor.execute('SELECT * FROM vpns WHERE city = ?', (call,)).fetchone()
+            return self.cursor.execute('SELECT * FROM FileOVPN WHERE city = ?', (call,)).fetchone()
 
     def in_zip(self, call):
         with self.connection:
-            return self.cursor.execute('SELECT * FROM vpns WHERE zipCode = ?', (call,)).fetchone()
+            return self.cursor.execute('SELECT * FROM FileOVPN WHERE zipCode = ?', (call,)).fetchone()
 
     def select_vpn(self, call):
         with self.connection:
-            return self.cursor.execute('SELECT * FROM vpns WHERE zipCode = ?', (call,)).fetchone()
+            return self.cursor.execute('SELECT * FROM FileOVPN WHERE zipCode = ?', (call,)).fetchone()
 
     def our_choice(self, city):
         with self.connection:
-            return self.cursor.execute('SELECT zipCode FROM vpns WHERE city = ?',(city,)).fetchall()
+            return self.cursor.execute('SELECT zipCode FROM FileOVPN WHERE city = ?',(city,)).fetchall()
 
     def count_rows(self):
         #Считаем количество строк
         with self.connection:
-            result = self.cursor.execute('SELECT * FROM vpns').fetchall()
-        
+            result = self.cursor.execute('SELECT * FROM FileOVPN').fetchall()
+
     def in_city(self, call):
         with self.connection:
-            return self.cursor.execute('SELECT * FROM vpns WHERE city = ?', (call,)).fetchone()
+            return self.cursor.execute('SELECT * FROM FileOVPN WHERE city = ?', (call,)).fetchone()
 
     def in_zip(self, call):
         with self.connection:
-            return self.cursor.execute('SELECT * FROM vpns WHERE zipCode = ?', (call,)).fetchone()
+            return self.cursor.execute('SELECT * FROM FileOVPN WHERE zipCode = ?', (call,)).fetchone()
 
     def select_namefile_none(self, call):
         with self.connection:
-            return self.cursor.execute('SELECT namefile FROM vpns WHERE city = ?', (call,)).fetchone()
+            return self.cursor.execute('SELECT file_name FROM FileOVPN WHERE city = ?', (call,)).fetchone()
 
     def select_namefile(self, call):
         with self.connection:
-            return self.cursor.execute('SELECT namefile FROM vpns WHERE city = ?', (call,)).fetchone()
+            return self.cursor.execute('SELECT file_name FROM FileOVPN WHERE city = ?', (call,)).fetchone()
 
     def our_choice(self, city):
         with self.connection:
-            return self.cursor.execute('SELECT zipCode FROM vpns WHERE city = ?',(city,)).fetchall()
+            return self.cursor.execute('SELECT zipCode FROM FileOVPN WHERE city = ?',(city,)).fetchall()
 
     def count_rows(self):
         #Считаем количество строк
         with self.connection:
-            result = self.cursor.execute('SELECT * FROM vpns').fetchall()
+            result = self.cursor.execute('SELECT * FROM FileOVPN').fetchall()
             return len(result)
 
     def exist(self, call):
         #просто нужная хуйня
         with self.connection:
-            return self.cursor.execute('SELECT ? FROM vpns',(call,)).fetchall()
+            return self.cursor.execute('SELECT ? FROM FileOVPN',(call,)).fetchall()
 
     def add_user(self, user_id):
         #добавляем пользователя
@@ -115,12 +118,12 @@ class Liter:
             return self.cursor.execute('SELECT choice FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
 
     def select_user_token(self, user_id):
-        
+
         with self.connection:
             return self.cursor.execute('SELECT token FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
 
     def select_user_payment_status(self, user_id):
-        
+
         with self.connection:
             return self.cursor.execute('SELECT payment_status FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
 
@@ -136,12 +139,12 @@ class Liter:
             self.cursor.execute('UPDATE users SET choice = ? WHERE user_id= ?',(choice[0], user_id))
 
     def update_user_token(self, user_id, token):
-     
+
         with self.connection:
             self.cursor.execute('UPDATE users SET token = ? WHERE user_id= ?',(token, user_id))
 
     def update_user_payment_status(self, user_id, status):
-       
+
         with self.connection:
             self.cursor.execute('UPDATE users SET payment_status = ? WHERE user_id= ?',(status, user_id))
 
@@ -151,9 +154,9 @@ class Liter:
             if choice == 'null' :
                 return 'null'
             else:
-                country = self.cursor.execute('SELECT countryName FROM vpns WHERE namefile = ?', (choice,)).fetchone()[0]
-                state = self.cursor.execute('SELECT stateProv FROM vpns WHERE namefile = ?', (choice,)).fetchone()[0]
-                city = self.cursor.execute('SELECT city FROM vpns WHERE namefile = ?', (choice,)).fetchone()[0]
+                country = self.cursor.execute('SELECT countryName FROM FileOVPN WHERE file_name = ?', (choice,)).fetchone()[0]
+                state = self.cursor.execute('SELECT stateProv FROM FileOVPN WHERE file_name = ?', (choice,)).fetchone()[0]
+                city = self.cursor.execute('SELECT city FROM FileOVPN WHERE file_name = ?', (choice,)).fetchone()[0]
             return country + ', ' + state + ', ' + city
 
     def close(self):
