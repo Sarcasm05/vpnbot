58498afad422aed0ad26fb40bd7a49f75cd3c260
monsherko <33561511+monsherko@users.noreply.github.com>
2018-06-23 01:55:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ sudo apt-get install mysql-server mysql-client
 echo "install libmysqlclient-dev"
 sudo apt-get install libmysqlclient-dev
 echo "please download package with pip and requeremnts.txt"
+599929762:AAEKzI9xHJ3IVDXr1_jXBbwpC3twpkNqhFc
