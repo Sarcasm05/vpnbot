d442dc3e75f72c96587b110ba58c73d2ce707b23
monsherko <monsherko@yahoo.com>
2018-06-23 13:08:43 +0300

    checking

--- src/keyboard.py
+++ src/keyboard.py
@@ -11,7 +11,7 @@ class Keyboard:
     def __init__(self, bot):
         self.bot = bot
     def hide(self):
-        markup = telebot.types.ReplyKeyboardRemove(True)
+        markup = telebot.types.ReplyKeyboardMarkup(True, True)
         return markup
 
     def country(self):
@@ -50,4 +50,3 @@ class Keyboard:
         markup.row('technical support')
         markup.row('donate')
         return markup
-
